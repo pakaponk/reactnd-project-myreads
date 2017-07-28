27b1adb9926bcd17b02d65f81a10ba9d06c414aa
Pakapon Kaewmark <sleep191@gmail.com>
2017-07-28 15:38:38 +0700

    refactor: Change Bookshelf and Book HTML part into component

---
+++ src/BookShelf.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Book from './Book';
+
+function BookShelf({ books, title }){
+	return (
+		<div className="bookshelf">
+			<h2 className="bookshelf-title">{ title }</h2>
+			<div className="bookshelf-books">
+				<ol className="books-grid">
+					{ books.map(book => (
+						<li key={book.id}>
+							<Book book={book} />
+						</li>
+					))}
+				</ol>
+			</div>
+		</div>
+	);
+}
+
+BookShelf.propTypes = {
+	title: PropTypes.string.isRequired,
+	books: PropTypes.array.isRequired
+};
+
+export default BookShelf;
